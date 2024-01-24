08efee738c6629857f6bb5b56b1446ceaa25c95a
AnnaKalis <annkalis@gmail.com>
2024-01-24 21:00:05 +0100

    Moving loader from global into function

--- js/pages/blogPost.js
+++ js/pages/blogPost.js
@@ -2,24 +2,24 @@ import { displayError } from "../ui/displayError.js";
 
 const postContainer = document.querySelector(".blog-post");
 const loaderContainer = document.createElement("div");
-loaderContainer.classList.add("loader");
-postContainer.append(loaderContainer);
 
 function createPost(details) {
-    loaderContainer.classList.remove("loader");
-    const postTitle = document.createElement("h1");
-    const postText = document.createElement("div");
-    postContainer.append(postTitle);
-    postContainer.append(postText);
-    postTitle.innerText = details.title.rendered;
-    postText.innerHTML = details.content.rendered;
-    console.log(details);
+  loaderContainer.classList.remove("loader");
+  const postTitle = document.createElement("h1");
+  const postText = document.createElement("div");
+  postContainer.append(postTitle);
+  postContainer.append(postText);
+  postTitle.innerText = details.title.rendered;
+  postText.innerHTML = details.content.rendered;
+  console.log(details);
 }
 
 export async function setUpBlogPostPage() {
   const queryString = document.location.search;
   const params = new URLSearchParams(queryString);
   const id = params.get("id");
+  loaderContainer.classList.add("loader");
+  postContainer.append(loaderContainer);
 
   const url = "https://thrive.annakalis.com/wp-json/wp/v2/posts/" + id + "?_embed";
 
@@ -33,7 +33,4 @@ export async function setUpBlogPostPage() {
   } catch (error) {
     postContainer.innerText = displayError("An error occured when uploading the product from the server!");
   }
-
-  
-  
 }
