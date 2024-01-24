170eb5e046c7a2080de6755a21d503b2b2cdc3f1
AnnaKalis <annkalis@gmail.com>
2024-01-24 20:07:18 +0100

    Adding API data to the single post

--- js/pages/blogPost.js
+++ js/pages/blogPost.js
@@ -1,13 +1,18 @@
 import { displayError } from "../ui/displayError.js";
 
+
+
 export async function setUpBlogPostPage (){
     const queryString = document.location.search;
     const params = new URLSearchParams(queryString);
     const id = params.get("id");
-    const postContainer = document.querySelector(".post");
-    postContainer.innerHTML = `<div class="loader"></div>`;
+   
+    const postContainer = document.querySelector(".blog-post");
+    const loaderContainer = document.createElement("div");
+    loaderContainer.classList.add("loader");
+    postContainer.append(loaderContainer);
 
-    const url = "https://thrive.annakalis.com/wp-json/wp/v2/posts/" + id;
+    const url = "https://thrive.annakalis.com/wp-json/wp/v2/posts/" + id + "?_embed";
 
     try {
         const response = await fetch(url);
@@ -19,13 +24,18 @@ export async function setUpBlogPostPage (){
         const getResult = await response.json();
         createPost(getResult);
     } catch (error) {
-        postContainer.innerHTML = displayError("An error occured when uploading the product from the server!");
+        postContainer.innerText = displayError("An error occured when uploading the product from the server!");
       }
        
-
     function createPost(details) {
-        postContainer.innerHTML = "";
-        postContainer.innerHTML += `<div>${details.title.rendered}</div>`;
+        loaderContainer.classList.remove("loader");
+        const postTitle = document.createElement("h1");
+        const postText = document.createElement("div");
+        postContainer.append(postTitle);
+        postContainer.append(postText);
+        postTitle.innerText = details.title.rendered;
+        postText.innerHTML = details.content.rendered;
+        console.log(details);
     }
 
 
