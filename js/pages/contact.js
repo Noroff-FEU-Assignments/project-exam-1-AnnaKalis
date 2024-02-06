c61b6a94f981c0b36cdc8b39f7894213d396920e
AnnaKalis <annkalis@gmail.com>
2024-02-06 22:10:11 +0100

    event listener on submit button

--- js/pages/contact.js
+++ js/pages/contact.js
@@ -3,7 +3,12 @@ import { validateForm } from "../ui/validateForm.js";
 export function setUpContactPage() {
   let form = document.getElementById("form");
   console.log(form);
-  form.addEventListener("submit", function (event) {
+  let submitButton = document.getElementById("submit-form-btn");
+  console.log(submitButton);
+  submitButton.addEventListener("click", function(event)
+
+//   form.addEventListener("submit", function (event) 
+  {
     console.log("Form submittet");
     event.preventDefault();
     validateForm();
